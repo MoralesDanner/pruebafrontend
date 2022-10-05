7effedf256956d2f132b114d324de6ff8562aa02
MoraleDanner <115031487+MoralesDanner@users.noreply.github.com>
2022-10-04 21:36:53 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://moralesdanner.github.io/pruebafrontend/index.php';
+$config['base_url'] = 'https://moralesdanner.github.io/pruebafrontend/';
 
 /*
 |--------------------------------------------------------------------------
